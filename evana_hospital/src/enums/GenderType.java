5a2e47c17bbbb5b6a9661a67cfb2a44d7f8eb146
evuti16 <evanaseid5@gmail.com>
2025-06-09 11:26:00 +0300

    Add files via upload

---
+++ evana_hospital/src/enums/GenderType.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package enums;
+
+/**
+ *
+ * @author tsion
+ */
+public enum GenderType {
+    MALE(1),FE_MALE(2);
+    final int state;
+    GenderType(int state){
+        this.state=state;
+    }
+}
---
+++ evana_hospital/src/util/cookie.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import dto.userdto;
+
+/**
+ *
+ * @author tsion
+ */
+public class cookie {
+    public static userdto selectedUser;
+    
+}
